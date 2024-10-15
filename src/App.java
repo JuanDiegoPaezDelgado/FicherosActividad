ad1ac8fa8b9d846d7621bd8a53b0bfff979c7eba
JuanDiegoPaezDelgado <usr3859@salesianos-lacuesta.net>
2024-10-15 21:19:42 +0100

    App.java:creacion de variables e importaciones necesarias

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       try {
+        File inputFile = new File("./files/contenido.txt");
+                File outputFile = new File("./files/output.txt");
+                BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+                BufferedWriter finalWriter = new BufferedWriter(new FileWriter(outputFile));
+                String line;
+                int lineNumber = 1;
+                List<ProcessBuilder> processBuilders = new ArrayList<>();
+                List<File> outputFiles = new ArrayList<>();
+
+       } catch (Exception e) {
+        
+       } 
     }
 }
